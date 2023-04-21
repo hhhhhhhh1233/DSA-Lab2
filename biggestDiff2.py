c8ca5b45f04298217450108aac4dbbf5bd06a979
hhhhhhhh <58606478+hhhhhhhh1233@users.noreply.github.com>
2023-04-22 00:20:55 +0200

    Add files via upload

---
+++ biggestDiff2.py
@@ -0,0 +1,93 @@
+import random, math, time
+import matplotlib.pyplot as plt
+
+def verifyBiggestDiff(arr):
+    biggestDiff = arr[1] - arr[0]
+    candidates = [arr[0], arr[1]]
+    for i, iElem in enumerate(arr):
+        for jElem in arr[i+1:]:
+            if jElem - iElem > biggestDiff:
+                candidates = [iElem, jElem]
+                biggestDiff = jElem - iElem
+    return candidates
+
+def BiggestDiffCandidate(arr):
+    if len(arr) == 2:
+        return arr
+    
+    lArr = BiggestDiffCandidate(arr[:len(arr)//2])
+    rArr = BiggestDiffCandidate(arr[len(arr)//2:])
+
+    wholeArr = lArr + rArr
+    #print(f"\nwholeArr: {wholeArr}")
+    candidates = [wholeArr[0], wholeArr[1]]
+    biggestDiff = wholeArr[1] - wholeArr[0]
+    candidatesPos = [0, 1]	
+    for i, iElem in enumerate(wholeArr):
+        for j, jElem in enumerate(wholeArr[i+1:]):
+            if jElem - iElem > biggestDiff:
+                candidates = [iElem, jElem]
+                biggestDiff = jElem - iElem
+                candidatesPos = [i, 1+j+i]
+                #print("The candidates ", wholeArr[i], wholeArr[j+i])
+            #print(iElem,jElem)
+
+
+    #print(f"Current candidates: {candidates}")
+    #print(f"candidatesPos: {candidatesPos}")
+    #print(f"Searching for a lower candidate in {wholeArr[candidatesPos[1]+1:]}")
+    backupI = wholeArr[candidatesPos[1]]
+    for i in wholeArr[candidatesPos[1]+1:]:
+        if i < backupI:
+            backupI = i
+
+    #print(f"Lower candidate: {backupI}")
+    candidates.append(backupI)
+    
+    #print(f"Searching for a higher candidate in {wholeArr[:candidatesPos[0]]}")
+    backupJ = wholeArr[candidatesPos[0]]
+    for j in wholeArr[:candidatesPos[0]]:
+        if j > backupJ:
+            backupJ = j
+			
+    #print(f"Higher candidate: {backupJ}")
+    candidates.insert(0, backupJ)
+
+    #print(f"Added backup candidates: {candidates}")
+
+    return candidates
+	
+testTimeTaken = []
+arraySize = []
+for i in range(15):
+    testArr = []
+    for j in range(int(math.pow(2,i+1))):
+        testArr.append(random.randint(-99,99))
+
+    startTime = time.time()
+    arr = BiggestDiffCandidate(testArr)
+    testTimeTaken.append(time.time() - startTime)
+    arraySize.append(math.pow(2,i+1))
+
+    if verifyBiggestDiff(arr) != verifyBiggestDiff(testArr):
+        print("\nERROR: Did not return expected result!")
+        quit()
+print("Correct!")
+
+plt.plot(arraySize,testTimeTaken)
+plt.show()
+
+#testArr = []
+#for i in range(16):
+#    testArr.append(random.randint(1,99))
+
+#print("WRONG SIZE") if math.log(len(testArr),2)%1 != 0 else print("Acceptable size")
+#print(f"Searching array: {testArr}")
+
+#arr = BiggestDiffCandidate(testArr)
+
+#print(f"\nIt's within this chunk {arr}")
+
+#print(f"The best candidates: {verifyBiggestDiff(testArr)}")
+
+#print("Correct!") if verifyBiggestDiff(arr) == verifyBiggestDiff(testArr) else print("Wrong!")
---
+++ smallestNum.py
@@ -0,0 +1,55 @@
+import random
+
+def SmallestNumInc(arr):
+    if len(arr) == 1:
+        return arr[0]
+    x = SmallestNum(arr[:-1])
+    if x > arr[-1]:
+        return arr[-1]
+    else:
+        return x
+
+def SmallestNumDAC(arr):
+    if len(arr) == 1:
+        return arr[0]
+    l = SmallestNumDAC(arr[:len(arr)//2])
+    r = SmallestNumDAC(arr[len(arr)//2:])
+    if l > r:
+        return r
+    else:
+        return l
+
+def SmallestThree(arr):
+    if len(arr) == 2:
+        if arr[0] < arr[1]:
+            return arr[0], arr[1]
+        else:
+            return arr[1], arr[0]
+    if len(arr) == 3:
+        x, y = SmallestThree(arr[:-1])
+        if arr[2] < y:
+            if arr[2] < x:
+                return arr[2],x,y
+            else:
+                return x,arr[2],y
+        else:
+            return x,y,arr[2]
+    x, y, z = SmallestThree(arr[:-1])
+    if arr[-1] < z:
+        if arr[-1] < y:
+            if arr[-1] < x:
+                return arr[-1], x, y
+            else:
+                return x, arr[-1], y
+        else:
+            return x, y, arr[-1]
+    else:
+        return x, y, z
+
+for i in range(10):
+    testArr = []
+    for j in range(10):
+        testArr.append(random.randint(0,30))
+    print(f"Arr: {testArr}\nSmallestThree: {SmallestThree(testArr)}\n")
+
+
